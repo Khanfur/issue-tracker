c6f6745c27852cd7d8f6c4a0018c22f300ec57a2
Brandon <brandon.tromp@gmail.com>
2024-02-13 10:52:22 +0100

    working on form handling of a new issue

--- app/components/issues/new-issue-dialog.tsx
+++ app/components/issues/new-issue-dialog.tsx
@@ -1,12 +1,21 @@
 'use client'
 
+import "easymde/dist/easymde.min.css";
 import { Button, Dialog, Flex, Text, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
 import Loading from '@/app/components/loading/loading'
 import { Suspense } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import axios, { Axios } from 'axios'
 
 export default function NewIssueDialog() {
+    const {
+        register,
+        control,
+        handleSubmit
+    } = useForm<IssueForm>()
+
+
     return (
         <Suspense fallback={ <Loading/> }>
             <Dialog.Root>
@@ -18,18 +27,28 @@ export default function NewIssueDialog() {
                     <Dialog.Title>Create a new issue</Dialog.Title>
 
                     <Flex direction="column" gap="3">
-                        <label>
-                            <Text as="div" size="2" mb="1" weight="bold">
-                                Email
-                            </Text>
-                            <TextField.Input placeholder="Enter your email"/>
-                        </label>
-                        <label>
-                            <Text as="div" size="2" mb="1" weight="bold">
-                                Description
-                            </Text>
-                            <SimpleMDE placeholder="Enter the descritpion of your issue"/>
-                        </label>
+                        <form onSubmit={ handleSubmit((data) => submitIssueForm(data)) }>
+                            <label>
+                                <Text as="div" size="2" mb="1" weight="bold">
+                                    Title
+                                </Text>
+                                <TextField.Input placeholder="Enter the title of the issue"
+                                                 { ...register('title') }
+                                />
+                            </label>
+                            <label>
+                                <Text as="div" size="2" mb="1" weight="bold">
+                                    Description
+                                </Text>
+
+                                <Controller render={ (field) =>
+                                    <SimpleMDE placeholder="Enter the descritpion of your issue" { ...field }/>
+                                }
+                                            name='description'
+                                            control={ control }
+                                />
+                            </label>
+                        </form>
                     </Flex>
 
                     <Flex gap="3" mt="4" justify="end">
@@ -46,4 +65,9 @@ export default function NewIssueDialog() {
             </Dialog.Root>
         </Suspense>
     )
+}
+
+async function submitIssueForm(data: IssueForm) {
+    console.log(data)
+    await axios.post('/api/issues', data)
 }
---
+++ app/interface/issues/issues.ts
@@ -0,0 +1,4 @@
+interface IssueForm {
+    title: string
+    description: string
+}
