0eb8c41ded369530e3db884d8b6331ef8420ce5c
Brandon <brandon.tromp@gmail.com>
2024-02-13 10:21:51 +0100

    added a simple loader

--- app/components/issues/new-issue-dialog.tsx
+++ app/components/issues/new-issue-dialog.tsx
@@ -1,45 +1,49 @@
 'use client'
 
-import { Dialog, Flex, TextField, Button, Text } from '@radix-ui/themes'
+import { Button, Dialog, Flex, Text, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import Loading from '@/app/components/loading/loading'
+import { Suspense } from 'react'
 
 export default function NewIssueDialog() {
     return (
-        <Dialog.Root>
-            <Dialog.Trigger>
-                <Button>New issue</Button>
-            </Dialog.Trigger>
+        <Suspense fallback={ <Loading/> }>
+            <Dialog.Root>
+                <Dialog.Trigger>
+                    <Button>New issue</Button>
+                </Dialog.Trigger>
 
-            <Dialog.Content style={{ maxWidth: 600 }}>
-                <Dialog.Title>Create a new issue</Dialog.Title>
+                <Dialog.Content style={ { maxWidth: 600 } }>
+                    <Dialog.Title>Create a new issue</Dialog.Title>
 
-                <Flex direction="column" gap="3">
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Email
-                        </Text>
-                        <TextField.Input placeholder="Enter your email"/>
-                    </label>
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Description
-                        </Text>
-                        <SimpleMDE placeholder="Enter the descritpion of your issue" />
-                    </label>
-                </Flex>
+                    <Flex direction="column" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Email
+                            </Text>
+                            <TextField.Input placeholder="Enter your email"/>
+                        </label>
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Description
+                            </Text>
+                            <SimpleMDE placeholder="Enter the descritpion of your issue"/>
+                        </label>
+                    </Flex>
 
-                <Flex gap="3" mt="4" justify="end">
-                    <Dialog.Close>
-                        <Button variant="soft" color="gray">
-                            Cancel
-                        </Button>
-                    </Dialog.Close>
-                    <Dialog.Trigger>
-                        <Button>Create issue</Button>
-                    </Dialog.Trigger>
-                </Flex>
-            </Dialog.Content>
-        </Dialog.Root>
+                    <Flex gap="3" mt="4" justify="end">
+                        <Dialog.Close>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </Dialog.Close>
+                        <Dialog.Trigger>
+                            <Button>Create issue</Button>
+                        </Dialog.Trigger>
+                    </Flex>
+                </Dialog.Content>
+            </Dialog.Root>
+        </Suspense>
     )
 }
---
+++ app/components/loading/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return(
+        <div className={ 'flex justify-center pt-5w' }>
+            <div className="border-gray-300 h-16 w-16 animate-spin rounded-full border-8 border-t-blue-600"/>
+        </div>
+    )
+}
