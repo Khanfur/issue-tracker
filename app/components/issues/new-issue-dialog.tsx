b478da0b6101370c27a0ec6d0db6dd5c585a6ae7
Brandon <brandon.tromp@gmail.com>
2024-02-13 15:56:09 +0100

    Solved some issues with the md editor

--- app/components/issues/new-issue-dialog.tsx
+++ app/components/issues/new-issue-dialog.tsx
@@ -2,11 +2,22 @@
 
 import "easymde/dist/easymde.min.css";
 import { Button, Dialog, Flex, Text, TextField } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
 import Loading from '@/app/components/loading/loading'
 import { Suspense } from 'react'
 import { Controller, useForm } from 'react-hook-form'
-import axios, { Axios } from 'axios'
+import axios from 'axios'
+
+import dynamic from "next/dynamic";
+
+const SimpleMdeEditor = dynamic(
+    () => import("react-simplemde-editor"),
+    { ssr: false }
+);
+
+async function submitIssueForm(data: IssueForm) {
+    console.log('submit', data)
+    await axios.post('/api/issues', data)
+}
 
 export default function NewIssueDialog() {
     const {
@@ -22,12 +33,11 @@ export default function NewIssueDialog() {
                 <Dialog.Trigger>
                     <Button>New issue</Button>
                 </Dialog.Trigger>
+                <form onSubmit={ handleSubmit((data) => submitIssueForm(data)) }>
+                    <Dialog.Content style={ { maxWidth: 600 } }>
+                        <Dialog.Title>Create a new issue</Dialog.Title>
 
-                <Dialog.Content style={ { maxWidth: 600 } }>
-                    <Dialog.Title>Create a new issue</Dialog.Title>
-
-                    <Flex direction="column" gap="3">
-                        <form onSubmit={ handleSubmit((data) => submitIssueForm(data)) }>
+                        <Flex direction="column" gap="3">
                             <label>
                                 <Text as="div" size="2" mb="1" weight="bold">
                                     Title
@@ -41,33 +51,37 @@ export default function NewIssueDialog() {
                                     Description
                                 </Text>
 
-                                <Controller render={ (field) =>
-                                    <SimpleMDE placeholder="Enter the descritpion of your issue" { ...field }/>
-                                }
-                                            name='description'
-                                            control={ control }
-                                />
-                            </label>
-                        </form>
-                    </Flex>
+                                <Controller render={({ field }) => {
+                                    const {
+                                        ref,
+                                        ...fieldProps
+                                    } = field
 
-                    <Flex gap="3" mt="4" justify="end">
-                        <Dialog.Close>
-                            <Button variant="soft" color="gray">
-                                Cancel
-                            </Button>
-                        </Dialog.Close>
-                        <Dialog.Trigger>
-                            <Button>Create issue</Button>
-                        </Dialog.Trigger>
-                    </Flex>
-                </Dialog.Content>
+                                    return (
+                                        <SimpleMdeEditor
+                                            placeholder="Enter the descritpion of your issue"
+                                            { ...fieldProps }
+                                        />
+                                    )
+                                }}
+                                        name="description"
+                                        control={control}
+                                        />
+                            </label>
+
+                        </Flex>
+
+                        <Flex gap="3" mt="4" justify="end">
+                            <Dialog.Close>
+                                <Button variant="soft" color="gray">
+                                    Cancel
+                                </Button>
+                            </Dialog.Close>
+                            <Button type='submit'>Create issue</Button>
+                        </Flex>
+                    </Dialog.Content>
+                </form>
             </Dialog.Root>
         </Suspense>
     )
-}
-
-async function submitIssueForm(data: IssueForm) {
-    console.log(data)
-    await axios.post('/api/issues', data)
 }
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { Suspense } from 'react'
 import NewIssueDialog from '@/app/components/issues/new-issue-dialog'
+import Loading from '@/app/components/loading/loading'
 
 export default function IssuesPage() {
     return (
         <div>
-            <NewIssueDialog/>
+            <Suspense fallback={ <Loading/> }>
+                <NewIssueDialog/>
+            </Suspense>
         </div>
     )
 }
