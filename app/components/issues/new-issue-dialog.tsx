d17dc4648cde74a1508bd172100056fb21702a02
Brandon <brandon.tromp@gmail.com>
2024-02-14 10:10:51 +0100

    new issue form can now be created

--- app/components/issues/new-issue-dialog.tsx
+++ app/components/issues/new-issue-dialog.tsx
@@ -2,8 +2,6 @@
 
 import "easymde/dist/easymde.min.css";
 import { Button, Dialog, Flex, Text, TextField } from '@radix-ui/themes'
-import Loading from '@/app/components/loading/loading'
-import { Suspense } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import axios from 'axios'
 
@@ -15,7 +13,6 @@ const SimpleMdeEditor = dynamic(
 );
 
 async function submitIssueForm(data: IssueForm) {
-    console.log('submit', data)
     await axios.post('/api/issues', data)
 }
 
@@ -26,62 +23,63 @@ export default function NewIssueDialog() {
         handleSubmit
     } = useForm<IssueForm>()
 
-
     return (
-        <Suspense fallback={ <Loading/> }>
-            <Dialog.Root>
-                <Dialog.Trigger>
-                    <Button>New issue</Button>
-                </Dialog.Trigger>
-                <form onSubmit={ handleSubmit((data) => submitIssueForm(data)) }>
-                    <Dialog.Content style={ { maxWidth: 600 } }>
-                        <Dialog.Title>Create a new issue</Dialog.Title>
+        <Dialog.Root>
+            <Dialog.Trigger>
+                <Button>New issue</Button>
+            </Dialog.Trigger>
+            <Dialog.Content style={ { maxWidth: 600 } }>
+                <form onSubmit={ handleSubmit(async (data, event) => await submitIssueForm(data)) }
+                >
+                    <Dialog.Title>Create a new issue</Dialog.Title>
 
-                        <Flex direction="column" gap="3">
-                            <label>
-                                <Text as="div" size="2" mb="1" weight="bold">
-                                    Title
-                                </Text>
-                                <TextField.Input placeholder="Enter the title of the issue"
-                                                 { ...register('title') }
-                                />
-                            </label>
-                            <label>
-                                <Text as="div" size="2" mb="1" weight="bold">
-                                    Description
-                                </Text>
+                    <Flex direction="column" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Title
+                            </Text>
+                            <TextField.Input placeholder="Enter the title of the issue"
+                                             { ...register('title') }
+                            />
+                        </label>
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Description
+                            </Text>
 
-                                <Controller render={({ field }) => {
-                                    const {
-                                        ref,
-                                        ...fieldProps
-                                    } = field
+                            <Controller render={ ({ field }) => {
+                                const {
+                                    ref,
+                                    ...fieldProps
+                                } = field
 
-                                    return (
-                                        <SimpleMdeEditor
-                                            placeholder="Enter the descritpion of your issue"
-                                            { ...fieldProps }
-                                        />
-                                    )
-                                }}
+                                return (
+                                    <SimpleMdeEditor
+                                        placeholder="Enter the descritpion of your issue"
+                                        { ...fieldProps }
+                                    />
+                                )
+                            } }
                                         name="description"
-                                        control={control}
-                                        />
-                            </label>
+                                        control={ control }
+                            />
+                        </label>
 
-                        </Flex>
+                    </Flex>
 
-                        <Flex gap="3" mt="4" justify="end">
-                            <Dialog.Close>
-                                <Button variant="soft" color="gray">
-                                    Cancel
-                                </Button>
-                            </Dialog.Close>
+                    <Flex gap="3" mt="4" justify="end">
+                        <Dialog.Close>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </Dialog.Close>
+                        <Dialog.Close>
                             <Button type='submit'>Create issue</Button>
-                        </Flex>
-                    </Dialog.Content>
+                        </Dialog.Close>
+                    </Flex>
                 </form>
-            </Dialog.Root>
-        </Suspense>
+            </Dialog.Content>
+
+        </Dialog.Root>
     )
 }
